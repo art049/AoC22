1fbd8b63c60dc29eec48f4491eadfa0f0260440f
Arthur Pastel <arthur.pastel@gmail.com>
2022-12-04 13:19:12 +0100

    improve the template to include benchmarks

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,9 +1,12 @@
+#![feature(test)]
+extern crate test;
+
 use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
 
-const DAY: &str = "01";
+const DAY: &str = "XX";
 
 fn get_input() -> impl Iterator<Item = String> {
     let path = format!("inputs/day{}.txt", DAY);
@@ -11,15 +14,36 @@ fn get_input() -> impl Iterator<Item = String> {
     BufReader::new(file).lines().filter_map(|line| line.ok())
 }
 
-fn part1() {
-    let lines = get_input();
+fn part1(lines: &impl Iterator<Item = String>) -> u32 {
+    0
 }
-
-fn part2() {
-    let lines = get_input();
+fn part2(lines: &impl Iterator<Item = String>) -> u32 {
+    0
 }
 
 fn main() {
-    part1();
-    part2();
+    let input = get_input();
+    let p1_total = part1(&input);
+    println!("Part1 total: {}", p1_total);
+    let p2_total = part2(&input);
+    println!("Part2 total: {}", p2_total);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn bench_part1(b: &mut Bencher) {
+        let lines: Vec<String> = get_input().collect();
+        b.iter(|| part1(&black_box(lines.to_vec().into_iter())));
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        let lines: Vec<String> = get_input().collect();
+        b.iter(|| part2(&black_box(lines.to_vec().into_iter())));
+    }
 }
